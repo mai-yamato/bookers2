1cfea6358d381d815e4e40c31f93414f3866f3ba
mai-yamato <my512.1515@gmail.com>
2021-05-15 12:18:51 +0000

    therd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_063512) do
+ActiveRecord::Schema.define(version: 2021_05_14_030943) do
+
+  create_table "bookers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.text "title"
