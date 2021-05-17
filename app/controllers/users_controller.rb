e2843235ea8c8faa485dfaaad3d90ed986a17eca
mai-yamato <my512.1515@gmail.com>
2021-05-17 13:25:27 +0000

    fifth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
   def show
   
   end
+  
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users
 
+  get "about" => "top#about" ,as:"about"
+
 
 end
